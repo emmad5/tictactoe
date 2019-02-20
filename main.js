9396d49a4878ee4a2258a459521e852102647178
Emma Deas <emmadeas9@gmail.com>
2019-02-20 11:47:37 -0800

    convert to class structure

--- main.js
+++ main.js
@@ -1,75 +1,78 @@
-document.addEventListener('DOMContentLoaded', loadGame);
-let isX = true;
+document.addEventListener('DOMContentLoaded', () => new Board());
 
-
-function loadGame() {
-    const squares = Array.from(document.querySelectorAll('.square'));
-    const button = document.querySelector('.button');
-    button.addEventListener('click', () => clearBoard(squares));
-    for (let i = 0; i < squares.length; i++) {
-        let square = squares[i]
-        square.addEventListener('click', () => playTurn(square, i, squares));
-    }  
-}
-function clearBoard(squares) {
-    for (let i = 0; i < squares.length; i++) {
-        let square = squares[i];
-        square.innerHTML = '';
+class Board {
+    constructor() {
+        this.board = document.querySelectorAll('.square');
+        console.log(document.querySelectorAll('.square'));
+        this.button = document.querySelector('.button');
+        this.playTurn = this.playTurn.bind(this);
+        this.isX = true;
+        this.button.addEventListener('click', () => this.clearBoard());
+            for (let i = 0; i < this.board.length; i++) {
+                let square = this.board[i];
+                square.addEventListener('click', () => this.playTurn(square, i));
+            }  
     }
-}
-function playTurn(square, i, squares) {
-    console.log(getPosition(i, squares));
-    if (!isOver(squares)) {
-        let mark = document.createElement('div')
-        if (square.innerHTML === '') {
-            if (isX) {
-                mark.innerHTML = 'x';
-                isX = false;
-                square.appendChild(mark);
-            } else {
-                mark.innerHTML = 'o';
-                isX = true;
-                square.appendChild(mark);
+
+    playTurn(square, i) {
+        console.log(this.getPosition(i));
+        if (!this.isOver(this.board)) {
+            let mark = document.createElement('div');
+            if (square.innerHTML === '') {
+                if (this.isX) {
+                    mark.innerHTML = 'x';
+                    this.isX = false;
+                    square.appendChild(mark);
+                } else {
+                    mark.innerHTML = 'o';
+                    this.isX = true;
+                    square.appendChild(mark);
+                }
             }
+        } else {
+            alert('game over');
+            this.clearBoard();
         }
-    } else {
-        alert('game over');
-        clearBoard(squares)
+    }
+    isOver() {
+        if (this.getPosition(0, this.board) === this.getPosition(1, this.board) && this.getPosition(0, this.board) === this.getPosition(2, this.board)) {
+            return true;
+        }
+        if (this.getPosition(3, this.board) === this.getPosition(4, this.board) && this.getPosition(3, this.board) === this.getPosition(5, this.board)) {
+            return true;
+        }
+        if (this.getPosition(6, this.board) === this.getPosition(7, this.board) && this.getPosition(7, this.board) === this.getPosition(8, this.board)) {
+            return true;
+        }
+        if (this.getPosition(0, this.board) === this.getPosition(3, this.board) && this.getPosition(0, this.board) === this.getPosition(6, this.board)) {
+            return true;
+        }
+        if (this.getPosition(1, this.board) === this.getPosition(4, this.board) && this.getPosition(1, this.board) === this.getPosition(7, this.board)) {
+            return true;
+        }
+        if (this.getPosition(2, this.board) === this.getPosition(5, this.board) && this.getPosition(2, this.board) === this.getPosition(8, this.board)) {
+            return true;
+        }
+        if (this.getPosition(0, this.board) === this.getPosition(4, this.board) && this.getPosition(0, this.board) === this.getPosition(8, this.board)) {
+            return true;
+        }
+        if (this.getPosition(2, this.board) === this.getPosition(4, this.board) && this.getPosition(2, this.board) === this.getPosition(6, this.board)) {
+            return true;
+        }
+    }
+
+    clearBoard() {
+        for (let i = 0; i < this.board.length; i++) {
+            let square = this.board[i];
+            square.innerHTML = '';
+        }
+    }
+
+    getPosition(index) {
+        if(this.board[index].childElementCount === 0 ) {
+            return index;
+        } else {
+            return this.board[index].innerHTML;
+        }
     }
 }
-
-function isOver(squares) {
-    if (getPosition(0, squares) === getPosition(1, squares) && getPosition(0, squares) === getPosition(2, squares)) {
-        return true;
-    }
-    if (getPosition(3, squares) === getPosition(4, squares) && getPosition(3, squares) === getPosition(5, squares)) {
-        return true;
-    }
-    if (getPosition(6, squares) === getPosition(7, squares) && getPosition(7, squares) === getPosition(8, squares)) {
-        return true;
-    }
-    if (getPosition(0, squares) === getPosition(3, squares) && getPosition(0, squares) === getPosition(6, squares)) {
-        return true;
-    }
-    if (getPosition(1, squares) === getPosition(4, squares) && getPosition(1, squares) === getPosition(7, squares)) {
-        return true;
-    }
-    if (getPosition(2, squares) === getPosition(5, squares) && getPosition(2, squares) === getPosition(8, squares)) {
-        return true;
-    }
-    if (getPosition(0, squares) === getPosition(4, squares) && getPosition(0, squares) === getPosition(8, squares)) {
-        return true;
-    }
-    if (getPosition(2, squares) === getPosition(4, squares) && getPosition(2, squares) === getPosition(6, squares)) {
-        return true;
-    }
-}
-
-function getPosition(index, squares) {
-   if(squares[index].childElementCount === 0 ) {
-       return index;
-   } else {
-       return squares[index].innerHTML;
-   }
-}
-
